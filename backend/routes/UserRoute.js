8a259c9328ab8b2d0185b7fda360422a11a6ef92
yeremiavictor <yeremiavictor@gmail.com>
2022-06-14 15:53:01 +0700

    fix misstype on router.patch on delete function

--- backend/routes/UserRoute.js
+++ backend/routes/UserRoute.js
@@ -13,6 +13,6 @@ router.get('/users', getUsers);
 router.get('/users/:id', getUserById);
 router.post('/users', createUser)
 router.patch('/users/:id', updateUser)
-router.patch('/users/:id', deleteUser)
+router.delete('/users/:id', deleteUser)
 
 export default router;
--- readme.md
+++ readme.md
@@ -2,3 +2,8 @@ Project Sederhana fullstack
 1. Backend dengan: Express
 2. Database dengan: Mysql
 3. Frontend dengan: React.js
+
+Cara menggunakan:
+1. Buat database di server anda: crud_db
+2. masuk dalam folder backend dg terminal, ketikkan nodemon start
+3. 
