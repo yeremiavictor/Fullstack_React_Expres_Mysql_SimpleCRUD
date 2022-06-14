c47b3b4c61a7e4afe65a7deb955947f2b849a119
yeremiavictor <yeremiavictor@gmail.com>
2022-06-14 14:23:54 +0700

    Create controlller

---
+++ backend/controllers/UserController.js
@@ -0,0 +1,58 @@
+import User from "../models/UserModel";
+
+export const getUsers = async(req,res) => {
+    try {
+        const response = await User.findAll()
+        res.status(200).json(response)
+    }catch(err){
+        console.log (err.message)
+    }
+}
+
+export const getUserById = async(req, res) => {
+    try{
+        const response = await User.findOne({
+            where:{
+                id: req.params.id
+            }
+        })
+        res.status(200).json(response)
+    } catch(err){
+        console.log(err.message)
+    }
+}
+
+export const createUser = async(req, res) => {
+    try{
+        await User.create(req.body)
+        res.status(201).json({msg: "User Created"})
+    } catch (err){
+        console.log(err.message)
+    }
+}
+
+export const updateUser = async(req,res) => {
+    try{
+        await User.update(req.body, {
+            where:{
+                id: req.params.id
+            }
+        })
+        res.status(200).json({msg: "User Updated"})
+    }catch(err){
+        console.log(err.message)
+    }
+}
+
+export const deleteUser = async(req, res) =>{
+    try{
+        await User.destroy({
+            where:{
+                id: req.params.id
+            }
+        })
+        res.status(200).json({msg:"User Deleted"})
+    }catch(err){
+        console.log(err.message)
+    }
+}
